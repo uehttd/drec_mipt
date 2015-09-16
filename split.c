d6a6cfe678def4de09603e1920d9d9109cf46ce9
VladMiryaha <vlad.miryaha@gmail.com>
2015-09-16 17:31:18 +0300

    ok

--- split.c
+++ split.c
@@ -13,14 +13,9 @@ int main()
 	printf("Enter the string:\n");
 	fgets(&string[0], DEF_STRING_LENGTH, stdin); 
 	
-	
 	char delimeters[] = " ,.-:?!\"\n\t;";
 	
 	char tokenData[DEF_STRING_LENGTH];
-  /*
-   * Не совсем понятны ваши пляски с двумя строчками. Что именно этим хотили сказать?
-   * Сохранили и исходную строку и модифицированную. Ок, пусть так.
-   */
 	memcpy(tokenData, string, sizeof(char) * DEF_STRING_LENGTH); //copying the string as it will be modified by the Split function
 	
 	char** tokens = (char**)calloc(DEF_TOKENS_COUNT, sizeof(char*));
@@ -33,37 +28,19 @@ int main()
 	{
 		printf("%s\n", tokens[i]);
 	}
-	
-	/*
-   * FIXIT:
-   * 1. не забывайте сами освобождать память за собой
-   * 2. компилятор выдаёт предупреждения, т.к. вы не подключили нужный заголовочный файл для calloc. 
-   */
-   	free(tokens);
+  free(tokens);
 	
 	return 0;
 }
 
 int Split(char* string, char* delimeters, char*** tokens, int* tokensCount)
 {
-	
-  /*
-   * FIXIT:
-   * Не круто когда функция выделяет какой-то кусок памяти, но сама не чистит за собой. Представьте, что вы хотите оформить вашу ф-ю в виде библиотеки, так что
-   * пользователь не увидит ваш код. Он видит только сигнатуру ф-и. Он каким-то образом должен догадаться, что память вы сами за него выделили, а освободить должен именно он.
-   * Если вы сделаете динамическую библиотеку, то такое точно сделать не получится (в разных местах выделить и освободить память).
-   * Надо вынести выделение памяти в main.
-   */
-	//*tokens = (char**)calloc(DEF_TOKENS_COUNT, sizeof(char*));
-	
 	int maxTokensCount = DEF_TOKENS_COUNT;
 	
 	*tokensCount = 0;
-	/*
-   * мне кажется, что *(*tokens + *tokensCount) куда менее понятно, чем (*tokens)[*tokensCount]
-   */
+
 	(*tokens)[*tokensCount] = strtok(string, delimeters);
-	while(*(*tokens + *tokensCount) != NULL)
+	while((*tokens)[*tokensCount] != NULL)
 	{
 		(*tokensCount)++;
 		if(*tokensCount >= maxTokensCount)
@@ -71,8 +48,6 @@ int Split(char* string, char* delimeters, char*** tokens, int* tokensCount)
 			*tokens = (char**)realloc(*tokens, sizeof(char*) * maxTokensCount * 2);
 			maxTokensCount *= 2;
 		}
-		
-		
 		(*tokens)[*tokensCount] = strtok(NULL, delimeters);
 	}
 	
